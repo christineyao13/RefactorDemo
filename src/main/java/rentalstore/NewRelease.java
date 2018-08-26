fe4977b28260dfe6d6114ec37cd6811262fd38bd
YAOCH2 <christine.s.yao@oocl.com>
2018-08-26 21:56:43 +0800

    [cy]rewrite getFrequentRenterPoints in NewRelease

--- src/main/java/rentalstore/NewRelease.java
+++ src/main/java/rentalstore/NewRelease.java
@@ -4,4 +4,12 @@ public class NewRelease extends MovieType{
     public double getAmount(double thisAmount, Rental each){
         return thisAmount+=each.getDayRented()*3;
     }
+    public int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+        frequentRenterPoints ++;
+        //add bonus for a two day new release rental
+        if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1){
+            frequentRenterPoints ++;
+        }
+        return frequentRenterPoints;
+    }
 }
